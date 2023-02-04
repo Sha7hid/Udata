b3e1ae5371e1c54aeaf9af3167c2e6931b12127e
sha7hid <shahidshafadshahad@gmail.com>
2023-02-04 20:25:07 +0530

    updt

--- fetch/src/App.js
+++ fetch/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Loading from './Loading';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
+
 
 function App() {
+  const [student, setStudent] = useState([]);
+  const [loading, setLoading] = useState(false);
+// const [num, setNum] = useState(false);
+  useEffect(()=>{
+    async function fetchdata(){
+      axios.get(`https://reqres.in/api/users?Page=1`).then(res =>{
+     setStudent(res.data.data);
+    
+    })
+}
+fetchdata();
+
+
+}
+  ,[])
+  console.log(student);
+  console.log(loading);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            Udata
+          </Navbar.Brand>
+          <button className='btn btn-warning' onClick={() => setLoading(true)} >Get users</button>
+        </Container>
+      </Navbar>
+     
+      <div className="grid-view">
+      
+{loading ? Array.from(student).map((student)=>(
+    //  <ListItem key={student.id} student={student}/>
+    <>
+  <div className='card-wrapper'>
+       <Card style={{ width: '15rem', height:'15rem' , display:'flex' , justifyContent:'center', alignItems:'center'}}>
+      <Card.Img style={{width:'10rem' ,height:'10rem'}} variant="top" src={student.avatar} />
+      <Card.Body>
+        <Card.Title>{student.first_name} {student.last_name}</Card.Title>
+        <Card.Text>
+         {student.email}
+        </Card.Text>
+      </Card.Body>
+    </Card>
     </div>
+    </>
+    )):<Loading/>}
+        </div>
+    </div>
+    
+
   );
 }
 
